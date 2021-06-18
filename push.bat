2634e2fb8fa843cf3f8e1614d0eb3e8f02602400
liwensong <wsliwh@163.com>
2021-06-18 16:17:25 +0800

    push.bat

---
+++ push.bat
@@ -0,0 +1,19 @@
+echo "Start submitting code to the local repository"
+echo "The current directory is：%cd%"
+git add *
+echo;
+
+echo "Commit the changes to the local repository"
+set now=%date% %time%
+echo %now%
+git commit -m "%now%"
+echo;
+
+echo "Commit the changes to the remote git server"
+git pull origin source
+echo;
+git push origin source
+echo;
+
+echo "Batch execution complete!"
+echo;
